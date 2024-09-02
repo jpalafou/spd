d58a7596315600900fe41e3d3218566a3ab5ace5
David Velasco <david.velasco@icf.unam.mx>
2024-09-02 14:39:19 -0400

    Added LHLLC Riemann solver

--- src/riemann_solver.py
+++ src/riemann_solver.py
@@ -143,3 +143,71 @@ class Riemann_solver:
         for vel in vels[1:]:
             F[vel,...] = F[_d_]*np.where(v_star>0,W_L[vel],W_R[vel])
         return F
+
+    def solve_riemann_lhllc(
+        self,
+        W_L: np.ndarray,
+        W_R: np.ndarray,
+        U_L: np.ndarray,
+        U_R: np.ndarray,
+        vels: np.array,
+        _p_: int,
+        gamma: float,
+        min_c2: float,
+        **kwargs,
+    ) -> np.ndarray:
+        #Density index
+        _d_ = 0
+        v_1 = vels[0]
+
+        c_L = hydro.compute_cs(W_L[_p_],W_L[_d_],gamma,min_c2) + np.abs(W_L[v_1])
+        c_R = hydro.compute_cs(W_R[_p_],W_R[_d_],gamma,min_c2) + np.abs(W_R[v_1])
+    
+        c_max = np.where(c_L>c_R,c_L,c_R)
+    
+        v_L = W_L[v_1]
+        v_R = W_R[v_1]
+        #Compute HLL wave speed
+        s_L = np.where(v_L<v_R,v_L,v_R)-c_max
+        s_R = np.where(v_L>v_R,v_L,v_R)+c_max
+
+        #Compute lagrangian sound speed
+        rc_L = W_L[_d_]*(v_L-s_L)
+        rc_R = W_R[_d_]*(s_R-v_R)
+
+        Mach  = np.minimum(1,np.maximum(np.abs(v_L),np.abs(v_R))/c_max)
+        phi   = Mach*(2-Mach)
+    
+        #Compute acoustic star state
+        v_star = (rc_R*v_R + rc_L*v_L + (W_L[_p_]-W_R[_p_]))/(rc_R+rc_L)
+        P_star = (rc_R*W_L[_p_] + rc_L*W_R[_p_] + phi*rc_L*rc_R*(v_L-v_R))/(rc_R+rc_L)
+    
+        #Left star region variables
+        r_starL = W_L[_d_]*(s_L-v_L)/(s_L-v_star)
+        e_starL =((s_L-v_L)*U_L[_p_]-W_L[_p_]*v_L+P_star*v_star)/(s_L-v_star)
+
+        #Right star region variables
+        r_starR = W_R[_d_]*(s_R-v_R)/(s_R-v_star)
+        e_starR = ((s_R-v_R)*U_R[_p_]-W_R[_p_]*v_R+P_star*v_star)/(s_R-v_star)
+
+        r_gdv = np.where(s_L>0,W_L[_d_],
+                    np.where(v_star>0,r_starL,
+                            np.where(s_R>0,r_starR,W_R[_d_])))
+        v_gdv = np.where(s_L>0,W_L[v_1],
+                        np.where(v_star>0,v_star,
+                                np.where(s_R>0,v_star,W_R[v_1])))
+        P_gdv = np.where(s_L>0,W_L[_p_],
+                        np.where(v_star>0,P_star,
+                                np.where(s_R>0,P_star,W_R[_p_])))
+        e_gdv = np.where(s_L>0,U_L[_p_],
+                        np.where(v_star>0,e_starL,
+                                np.where(s_R>0,e_starR,U_R[_p_])))
+          
+        F = W_L.copy()             
+        F[_d_,...] = r_gdv*v_gdv
+        F[v_1,...] = F[_d_]*v_gdv + P_gdv
+        F[_p_,...] = v_gdv*(e_gdv + P_gdv)
+        F[_p_] *= 0 if kwargs["isothermal"] else 1
+        for vel in vels[1:]:
+            F[vel,...] = F[_d_]*np.where(v_star>0,W_L[vel],W_R[vel])
+        return F
