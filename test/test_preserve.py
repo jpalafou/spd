6c0e5a681e13c2dff1ea9452188df762095c081f
David Velasco <david.velasco@icf.unam.mx>
2024-09-12 15:42:12 -0400

    Updated test_transforms

--- test/test_preserve.py
+++ test/test_preserve.py
@@ -10,10 +10,29 @@ N=6
 @pytest.mark.parametrize("p" , [1,3])
 @pytest.mark.parametrize("N" , [(N,),(N,N)])
 @pytest.mark.parametrize("update" , ["SD","FV"])
-def test_update(p,N,update):
+def test_update_sd(p,N,update):
     s = SDADER_Simulator(p=p,N=N,use_cupy=False,
                          update=update)
     s.perform_time_evolution(1)
     assert np.mean(np.abs(s.dm.W_cv[0]-s.W_init_cv[0])) > tolerance
     assert np.mean(np.abs(s.dm.W_cv[1:]-s.W_init_cv[1:])) < tolerance
     
+#Now for the FB scheme
+@pytest.mark.parametrize("p" , [1,3])
+@pytest.mark.parametrize("N" , [(N,),(N,N)])
+def test_update_fb(p,N):
+    s = SDADER_Simulator(p=p,N=N,use_cupy=False,
+                         update="FV",FB=True,godunov=True)
+    s.perform_time_evolution(1)
+    assert np.mean(np.abs(s.dm.W_cv[0]-s.W_init_cv[0])) > tolerance
+    assert np.mean(np.abs(s.dm.W_cv[1:]-s.W_init_cv[1:])) < tolerance
+
+#Now for the FB scheme
+@pytest.mark.parametrize("p" , [1,3])
+@pytest.mark.parametrize("N" , [(N,),(N,N)])
+def test_update(p,N):
+    s = SDADER_Simulator(p=p,N=N,use_cupy=False,
+                         update="FV",FB=True)
+    s.perform_time_evolution(1)
+    assert np.mean(np.abs(s.dm.W_cv[0]-s.W_init_cv[0])) > tolerance
+    assert np.mean(np.abs(s.dm.W_cv[1:]-s.W_init_cv[1:])) < tolerance
--- test/test_transforms.py
+++ test/test_transforms.py
@@ -6,6 +6,7 @@ import polynomials as poly
 from transforms import compute_A_from_B
 from transforms import compute_A_from_B_full
 from sd_simulator import SD_Simulator
+from itertools import repeat
 
 tolerance=1E-14
 N=6
@@ -14,7 +15,8 @@ N=6
 @pytest.mark.parametrize("ndim" , [1,2,3])
 @pytest.mark.parametrize("N"    , [1,N])
 def test_back_and_forth(p,N,ndim):
-    s = SD_Simulator(p=p,Nx=N,Ny=N,Nz=N,ndim=ndim)
+
+    s = SD_Simulator(p=p,N=tuple(repeat(N,ndim)))
     W = s.compute_cv_from_sp(s.compute_sp_from_cv(s.dm.W_cv))
     assert np.all(np.abs(W-s.dm.W_cv) < tolerance)
     W = s.compute_sp_from_fp(s.compute_fp_from_sp(s.dm.W_sp,"x"),"x")
@@ -24,7 +26,7 @@ def test_back_and_forth(p,N,ndim):
 @pytest.mark.parametrize("ndim" , [2])
 @pytest.mark.parametrize("N" , [1,N])
 def test_xyz_symmetry(p,N,ndim):
-    s = SD_Simulator(p=p,Nx=N,Ny=N,Nz=N,ndim=ndim)
+    s = SD_Simulator(p=p,N=tuple(repeat(N,ndim)))
     W_x = s.transpose_to_fv(s.compute_fp_from_sp(s.dm.W_sp,"x"))
     W_y = s.transpose_to_fv(s.compute_fp_from_sp(s.dm.W_sp,"y"))
     W_y = np.transpose(W_y,(0,2,1))
